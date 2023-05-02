08e4c59fa316ec654e881df3a2e457e3d74b9782
Saiyed Abbas Husain Asif <39983463+abbasasif@users.noreply.github.com>
2023-05-02 12:20:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,30 +44,55 @@ try:
 except URLError as e:
   streamlit.error()
   
+streamlit.header("The fruit load list contains:")
+#Snowflake related functions
+def get_fruit_load_list():
+  with cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit_load_list")
+    return my_cur.fetchall()
+
+#Add a button to load the fruit
+if streamlit.button('Get fruit load list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+#don't run anything past here while we troubleshoot
+
+#Allow the end user to add a fruit to the list:
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values('from streamlit')")
+    return "Thanks for adding "+new_fruit
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a fruit to the list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(fruityvice_response.json())
+
+streamlit.stop()  
   
-  
-  #streamlit.write('The user entered ', fruit_choice)
+  #streamlit.write(back_from_function)
 
 #import requests
-# streamlit.text(fruityvice_response.json())
+# 
 
 # Normalizing JSON response 
 # write your own comment - what does this do?
 
-#don't run anything past here while we troubleshoot
-streamlit.stop()
+
 
 #import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
+
+
+#my_cur = my_cnx.cursor()
+
+#streamlit.header("The fruit load list contains:")
+
+
+#add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+#streamlit.write('Thanks for adding ', add_my_fruit)
 
 #This will not work properly, but just go with the flow
-my_cur.execute("insert into fruit_load_list values('from streamlit')")
+
